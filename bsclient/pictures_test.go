07a0e80d0327669b3d982f89960d45e6702df95d
Dimitri <dns@masagroup.net>
2016-12-02 08:37:08 +0100

    fix pictures/shows go test

--- bsclient/pictures_test.go
+++ bsclient/pictures_test.go
@@ -12,11 +12,13 @@ func (s *MySuite) TestPicturesShows(c *C) {
 	c.Assert(err, IsNil)
 	picture, err := bs.PicturesShows(1, -1, -1)
 	c.Assert(err, IsNil)
-	c.Assert(len(picture), Equals, 50241)
+	// can't equals to a specific value since
+	// a different image can be retrieve somethimes
+	c.Assert(len(picture) > 0, Equals, true)
 
 	picture, err = bs.PicturesShows(1, 100, 100)
 	c.Assert(err, IsNil)
-	c.Assert(len(picture), Equals, 10986)
+	c.Assert(len(picture) > 0, Equals, true)
 
 	picture, err = bs.PicturesShows(0, 100, 100)
 	c.Assert(err, NotNil)
